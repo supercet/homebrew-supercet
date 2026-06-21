84dc049eb8e079f9810c917f6bfc6fe63d7686bb
myfancypants <11448979+myfancypants@users.noreply.github.com>
2026-06-21 05:57:52 +0000

    Update formula for version 0.74.1

--- Formula/autamo.rb
+++ Formula/autamo.rb
@@ -1,14 +1,14 @@
 class Autamo < Formula
   desc "Autamo - A Node.js application for git operations"
   homepage "https://autamo.ai"
-  version "0.74.0"
-  url "https://github.com/tryautamo/homebrew-tap/releases/download/v0.74.0/autamo-arm64"
-  sha256 "3a0ad7897c316f15080badde4cdf64cac634a8e8e98adfd36878fc096dce9b26"
+  version "0.74.1"
+  url "https://github.com/tryautamo/homebrew-tap/releases/download/v0.74.1/autamo-arm64"
+  sha256 "588697826163945d9c1316863125cbeac3485a9257f59edcd2d2382277ef79b4"
   license "MIT"
 
   on_arm do
-    url "https://github.com/tryautamo/homebrew-tap/releases/download/v0.74.0/autamo-arm64"
-    sha256 "3a0ad7897c316f15080badde4cdf64cac634a8e8e98adfd36878fc096dce9b26"
+    url "https://github.com/tryautamo/homebrew-tap/releases/download/v0.74.1/autamo-arm64"
+    sha256 "588697826163945d9c1316863125cbeac3485a9257f59edcd2d2382277ef79b4"
   end
 
   # on_intel do
